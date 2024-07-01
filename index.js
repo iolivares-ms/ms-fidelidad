73d5b7a12feeeffcb71aa7864d02e449b13c82dc
iolivares-ms <iolivares@mercantilseguros.com>
2024-07-01 11:44:05 -0400

    add pay frequency

--- index.js
+++ index.js
@@ -27,15 +27,25 @@ products = []
 threshold = [10,20,30]
 vm = 0.1
 am = 0.2
+fm = 0.1
 miu = 3
 
 function addProduct(){
     index = document.getElementById("products").value
     html = `<div class="p-card">
-                <h2>${ptypes[index].name}</h2>
+                <h2 id="product${products.length}">${ptypes[index].name}</h2>
                 <div class="flex-row">
                    <h3>Antiguedad (Años): </h3>
                    <input style="width: 15%;" type="number" value=1 onchange="updateProduct(${products.length},this.value,false)">
+                </div>
+                <div class="flex-row">
+                    <h3 for="products">Frecuencia de pago: </h3>
+                    <select name="fr" id="fr${products.length}" style="margin-left:0.5rem;" onchange="updateTier()">
+                        <option value=0> Mensual</option>
+                        <option value=1> Trimestral</option>
+                        <option value=2> Semestral</option>
+                        <option value=3> Anual</option>
+                    </select>
                 </div>
                 <div class="flex-row">
                     <h3>Puntos de Producto: </h3>
@@ -98,6 +108,10 @@ function updateHiper(index, newValue){
         miu = newValue
         document.getElementById("miu").innerHTML = miu
         break
+    case 3:
+        fm = newValue/100
+        document.getElementById("fm").innerHTML = fm.toFixed(2)
+        break
     }
     updateTier()
 }
@@ -106,17 +120,22 @@ function updateTier(){
    
     
     sumacc = 0
-    products.forEach(product =>{
+    products.forEach((product, index) =>{
         pa = 0
         products.forEach(auxproduct => {
             if (product.type === auxproduct.type){
                 pa +=1
             }
         })
+        
 
-        ant = 1+(product.ant*am)
+        ant = 1+(product.ant*am)+(fm*document.getElementById(`fr${index}`).value)
         log = Math.log10(product.pp)/Math.log10(miu*pa)
         sumacc += ant*log
+
+        totalProduct = ant*log
+        document.getElementById(`product${index}`).innerHTML =`${product.type} > ${totalProduct.toFixed(2)}pts`
+
     })
     productNames = []
     products.forEach(auxproduct => {
@@ -139,17 +158,17 @@ function updateTierPic(points){
     if (points < threshold[0]) {
         html = `<div>
         <img src="assets/silver.jpg" alt="silver" style="width:100px;height:100px;">
-            <p>Plata</p>
+            <h2>Plata</h2>
             </div>`
     } else if (points <threshold[1]){
         html =`<div><img src="assets/gold.png" alt="gold" style="width:100px;height:100px;">
-            <p>Oro</p></div>`
+            <h2>Oro</h2></div>`
     } else if (points < threshold[2]) {
         html =`<div><img src="assets/platinum.png" alt="platinum" style="width:100px;height:100px;">
-            <p>Platino</p></div>`
+            <h2>Platino</h2></div>`
     } else {
         html =`<div><img src="assets/diamond.png" alt="diamond" style="width:100px;height:100px;">
-            <p>Diamante</p></div>`
+            <h2>Diamante</h2></div>`
     }
 
     const node = new DOMParser().parseFromString(html, "text/html").body.firstElementChild;
@@ -158,3 +177,22 @@ function updateTierPic(points){
 }
 
 loadProducts()
+
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
